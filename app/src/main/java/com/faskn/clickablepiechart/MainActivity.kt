f5469ffb5c8786644afeac1a72d07a4756aaa66a
goksut <turkergoksu@gmail.com>
2020-08-30 20:39:03 +0300

    Added Kotlin DSL over Builder pattern.

--- app/src/main/java/com/faskn/clickablepiechart/MainActivity.kt
+++ app/src/main/java/com/faskn/clickablepiechart/MainActivity.kt
@@ -3,8 +3,8 @@ package com.faskn.clickablepiechart
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.faskn.lib.PieChart
 import com.faskn.lib.Slice
+import com.faskn.lib.pieChart
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -14,45 +14,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         chart.setCenterColor(R.color.white)
 
-        // Example
-        /* val pieChart0 = PieChart.Builder(
-             arrayOf(
-                 Slice(30f, R.color.colorPrimary),
-                 Slice(60f, R.color.colorPrimaryDark),
-                 Slice(120f, R.color.materialIndigo600),
-                 Slice(150f, R.color.colorAccent)
-             )
-         ).setSliceStartPoint(-90f)
-             .setClickListener { string, float ->
-                 Log.d("ses", "s " + string)
-                 Log.d("ses", "f " + float.toString())
-             }
-             .build()*/
-
-        // Example 2
-        val pieChart1 = PieChart.Builder(
-            arrayOf(
-                Slice(Random.nextInt(1000, 3000).toFloat(), R.color.colorPrimary),
-                Slice(Random.nextInt(1000, 2000).toFloat(), R.color.colorPrimaryDark),
-                Slice(Random.nextInt(1000, 5000).toFloat(), R.color.materialIndigo600),
-                Slice(Random.nextInt(1000, 10000).toFloat(), R.color.colorAccent)
-            )
-        )
-            .setClickListener { string, float ->
-                Log.d("ses", "s " + string)
-                Log.d("ses", "f " + float.toString())
+        // Kotlin DSL example
+        val pieChart1 = pieChart {
+            slices {
+                arrayOf(
+                    Slice(Random.nextInt(1000, 3000).toFloat(), R.color.colorPrimary),
+                    Slice(Random.nextInt(1000, 2000).toFloat(), R.color.colorPrimaryDark),
+                    Slice(Random.nextInt(1000, 5000).toFloat(), R.color.materialIndigo600),
+                    Slice(Random.nextInt(1000, 10000).toFloat(), R.color.colorAccent)
+                )
             }
-            .build()
 
-        // Example 3
-        /*val pieChart3 = PieChart.Builder(
-            null
-        )
-            .setClickListener { string, float ->
-                Log.d("ses", "s " + string)
-                Log.d("ses", "f " + float.toString())
+            sliceWidth { 80f }
+            sliceStartPoint { 0f }
+            clickListener { s, fl ->
+                Log.d("ses", "s " + s)
+                Log.d("ses", "f " + fl.toString())
             }
-            .build()*/
+        }
 
         chart.setPieChart(pieChart1)
     }
